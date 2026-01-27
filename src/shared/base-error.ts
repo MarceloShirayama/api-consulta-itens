3ab38a8546569e220b1396dbb7573d8292514254
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2026-01-27 16:58:38 -0300

    adicionado configuraçòes biome

--- src/shared/base-error.ts
+++ src/shared/base-error.ts
@@ -1,12 +1,15 @@
 export class BaseError extends Error {
-  name: string
-  message: string
-  cause?: { message: string; field: string }
+	name: string;
+	message: string;
+	cause?: { message: string; field: string };
 
-  constructor({ message, cause }: { message: string; cause?: { message: string; field: string } }) {
-    super(message)
-    this.message = message
-    this.name = `${this.constructor.name}-${Object.getPrototypeOf(this.constructor.prototype).constructor.name}`
-    this.cause = cause
-  }
+	constructor({
+		message,
+		cause,
+	}: { message: string; cause?: { message: string; field: string } }) {
+		super(message);
+		this.message = message;
+		this.name = `${this.constructor.name}-${Object.getPrototypeOf(this.constructor.prototype).constructor.name}`;
+		this.cause = cause;
+	}
 }
